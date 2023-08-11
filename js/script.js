f9e7d21fb5f2cd130ae285bf28d9e2fd84901a11
full-stack-david <davidchan12323@gmail.com>
2023-08-11 04:27:39 -0500

    Add Reviews section

--- js/script.js
+++ js/script.js
@@ -44,3 +44,20 @@ var swiper = new Swiper(".home-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+     640: {
+       slidesPerView: 1,
+     },
+     768: {
+       slidesPerView: 2,
+     },
+     991: {
+       slidesPerView: 3,
+     },
+  },
+});
