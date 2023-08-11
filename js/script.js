6a1452e5d3e5eef4f54aec2ae89cb68550dc60c2
full-stack-david <davidchan12323@gmail.com>
2023-08-11 04:37:50 -0500

    Add Logo section

--- js/script.js
+++ js/script.js
@@ -78,3 +78,23 @@ var swiper = new Swiper(".blogs-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".logo-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+     450: {
+        slidesPerView: 2,
+      },
+     640: {
+       slidesPerView: 3,
+     },
+     768: {
+       slidesPerView: 4,
+     },
+     1000: {
+       slidesPerView: 5,
+     },
+  },
+});
